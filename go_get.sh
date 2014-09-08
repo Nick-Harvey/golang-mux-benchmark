2bddb687ccc27013bdae3c9acebcd9473de03c4c
Nick Harvey <nickharveyonline@gmail.com>
2014-09-08 12:31:21 -0400

    Testing

---
+++ go_get.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "getting golang web"
+go get "github.com/gocraft/web"
+echo "getting gorilla mux"
+go get "github.com/gorilla/mux"
+echo "getting martini"
+go get "github.com/go-martini/martini"
+echo "getting tigertonic"
+go get "github.com/rcrowley/go-tigertonic"
+echo "getting traffic" 
+go get "github.com/pilu/traffic"
+
